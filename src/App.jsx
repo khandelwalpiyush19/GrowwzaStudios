b29b6c1e9347841cf1cd3efb81873ef43d2752dd
khandelwalpiyush19 <piyushkhandelwal482@gmail.com>
2026-02-27 10:51:54 +0530

    feat: Add ScrollToTop component for improved navigation experience

--- src/App.jsx
+++ src/App.jsx
@@ -7,12 +7,14 @@ import { Routes,Route } from 'react-router-dom'
 import PremiumServicesPage from './Components/Services'
 import PremiumContactPage from './Components/Contacts'
 import BlogPage from './Components/Blogs'
+import ScrollToTop from './Components/ScrollToTop'
 function App() {
   
 
   return (
     <>
 <Navbar className="sticky top-0 z-50"/>
+<ScrollToTop/>
      <Routes>
 <Route path="/" element={<LandingPage />} />
           <Route path="/about" element={<AboutUsPage />} />
